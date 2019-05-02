bdeed4c940a14f43d357d8099de20e7ad00af147
J. Singleton <jennifer.singleton@gmail.com>
2019-05-02 03:27:41 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -44,12 +44,10 @@ class SongsController < ApplicationController
   end
 
   patch '/songs/:id' do
-    binding.pry
-
     @song = Song.find(params[:id])
     @song.update(params[:song])
     @song.artist.update(params[:artist])
-
+binding.pry
     if !params[:genre]
       @song.genres << Genre.create(params[:genre])
       @song.save
