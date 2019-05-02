c65d88c9fde3aedcac284e7a1869f4c274fe8c46
J. Singleton <jennifer.singleton@gmail.com>
2019-05-02 02:27:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
-
+  enable :session_secret
   get '/' do
     erb :index
   end
